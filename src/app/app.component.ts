e14679a3d314f76fc318f8f64ca61055679039fb
Tirumala <tirumalatamma17@gmail.com>
2024-12-03 17:13:59 +0530

    Flutter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,20 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'webviewDemo';
 
-  onButtonClick(){
-    
-  }
+  getFlutterDeviceToken(): void {
+    if (window['flutter_inappwebview']) {
+      window['flutter_inappwebview']
+        .callHandler('getDeviceToken')
+        .then((token: string) => {
+          console.log('Received Device Token:', token);
+          alert('Device Token: ' + token);
+        })
+        .catch((error: any) => {
+          console.error('Error fetching device token:', error);
+        });
+    } else {
+      console.error('Flutter InAppWebView is not available.');
+      alert('Flutter InAppWebView is not available.');
+    }
+}
 }
---
+++ src/typing.d.ts
@@ -0,0 +1,6 @@
+
+interface Window {
+  flutter_inappwebview?: {
+    callHandler(handlerName: string, ...args: any[]): Promise<any>;
+};
+}
